699296c39a19b6395b4d2dde03f50c0d99b11b55
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2022-09-20 22:26:25 +0200

    Llave del nivel
    -Logica básica de la llave

---
+++ IndieSpainJamProjec/Assets/Scripts/General/LevelKey.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Clase que se encarga de que el jugador pueda obtener la llave y esta le siga
+public class LevelKey : MonoBehaviour
+{
+    [Header("[References]")]
+    public GameObject player;
+
+    [Header("[Configuration]")]
+    public float followSpeed;
+    public float followMinDistance;
+
+    [Header("[Values]")]
+    public bool keyObtained;
+    public bool followingPlayer;
+    private Transform lastPosition;
+    public List<Transform> playerPositionList;
+
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPositionList = new List<Transform>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && keyObtained == false)
+        {
+            keyObtained = true;
+            followingPlayer = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (followingPlayer && player != null)
+        {
+            float playerDistance = Vector2.Distance(transform.position, player.transform.position);
+
+            //Si el jugador se aleja de la llave, guardamos el camino que está siguiendo
+            if(playerDistance > followMinDistance)
+            {
+                playerPositionList.Add(player.transform);
+                transform.position = Vector3.Lerp(transform.position, playerPositionList[0].transform.position, followSpeed * Time.deltaTime);
+                lastPosition = playerPositionList[0];
+                playerPositionList.RemoveAt(0);
+            }
+
+            //Si hay al menos una posición en al lista el jugador se ha movido, movemos la llave hasta la siguiente posición
+            if (playerPositionList.Count > 0)
+                transform.position = Vector3.Lerp(transform.position, playerPositionList[0].transform.position, followSpeed * Time.deltaTime);
+            else
+                transform.position = Vector3.Lerp(transform.position, lastPosition.position, followSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ IndieSpainJamProjec/Assets/Scripts/Jugador/PlayerKeyChecker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Clase que se encarga de verificar si el jugador ha obtenido la llave del nivel
+public class PlayerKeyChecker : MonoBehaviour
+{
+    [Header("[Values]")]
+    public bool hasKey;
+
+
+    public bool Check_PlayerHasKey()
+    {
+        return hasKey;
+    }
+
+}
