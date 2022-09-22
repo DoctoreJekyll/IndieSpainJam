5a9b22dcb7410d763b66d5f4354314ed524b3f13
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-09-22 21:00:21 +0200

    Launcher

--- IndieSpainJamProjec/Assets/Scripts/General/Launcher.cs
+++ IndieSpainJamProjec/Assets/Scripts/General/Launcher.cs
@@ -15,28 +15,40 @@ public class Launcher : MonoBehaviour
         RIGTH
     }
 
+    [SerializeField] private float fireRate;
     [SerializeField] private float arrowSpeed;
     [SerializeField] private GameObject projectileArrow;
     [SerializeField] private LauncherDirection _launcherDirection = LauncherDirection.LEFT;
-
-
+    
     private void Start()
     {
-        
+        InvokeRepeating(nameof(SwitchBetweenLauncherTypes), 1, fireRate);
     }
 
-    private void Update()
+    private void SwitchBetweenLauncherTypes()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            LaunchProjectile(Vector2.up, Vector3.zero);
+        switch (_launcherDirection)
+        {    
+            case LauncherDirection.TOP:
+                LaunchProjectile(Vector2.up, Vector3.zero);
+                break;
+            case LauncherDirection.BOT:
+                LaunchProjectile(Vector2.down, new Vector3(0f,0f,-180f));
+                break;
+            case LauncherDirection.LEFT:
+                LaunchProjectile(Vector2.left, new Vector3(0f,0f,90f));
+                break;
+            case LauncherDirection.RIGTH:
+                LaunchProjectile(Vector2.right, new Vector3(0f,0f,-90f));
+                break;
+            
         }
     }
 
     private void LaunchProjectile(Vector2 direction, Vector3 rotationValues)
     {
         GameObject arrow = Instantiate(projectileArrow, transform.position, Quaternion.identity);
-        arrow.GetComponent<Rigidbody2D>().velocity = direction * (arrowSpeed * Time.deltaTime);
+        arrow.GetComponent<Rigidbody2D>().velocity = direction * arrowSpeed;
         arrow.GetComponent<Transform>().eulerAngles = rotationValues;
     }
 
