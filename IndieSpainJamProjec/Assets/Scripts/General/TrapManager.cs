7d2cda85cd0efffae36edc8ae4e5bf452bad57e3
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-09-22 12:55:54 +0200

    Traps

---
+++ IndieSpainJamProjec/Assets/Scripts/General/TrapManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapManager : MonoBehaviour
+{
+    [SerializeField] private bool isPlayerOnTrap;
+    [SerializeField] private GameObject checkObject;
+    [SerializeField] private Vector2 boxCheckSize;
+    [SerializeField] private LayerMask layerToCheckPlayer;
+
+    private void Update()
+    {
+        PlayerDetect();
+        CallToPlayerDeath();
+    }
+
+    private void PlayerDetect()
+    {
+        isPlayerOnTrap = Physics2D.OverlapBox(checkObject.transform.position, boxCheckSize, 0, layerToCheckPlayer);
+    }
+
+    private void CallToPlayerDeath()
+    {
+        if (isPlayerOnTrap)
+        {
+            PlayerDeath playerDeath = FindObjectOfType<PlayerDeath>().GetComponent<PlayerDeath>();
+            playerDeath.OnDeath();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(checkObject.transform.position, boxCheckSize);
+    }
+    
+    
+}
