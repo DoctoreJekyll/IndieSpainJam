a7c02542952cdf1c29586b4589dd321b9a7890eb
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-09-24 02:05:13 +0200

    sonidos en las temperaturas que salen del suelo, es una ñapa pero mira, la vida es dura
    Es definitivamente una cosa lo que he tenido que hacer xD

--- IndieSpainJamProjec/Assets/Scripts/Otros/TempChanger.cs
+++ IndieSpainJamProjec/Assets/Scripts/Otros/TempChanger.cs
@@ -14,27 +14,71 @@ public class TempChanger : MonoBehaviour
     [SerializeField] private AudioClip _audioClipFrost;
     private AudioSource _audioSource;
     private PlayerStatesManager _playerStatesManager;
-
-    public bool playerIsOn;
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         tempManager = GameObject.FindGameObjectWithTag("Temp Manager").GetComponent<TempManager>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            playerIsOn = true;
             float potencia = 1 / Vector3.Distance(this.transform.position, collision.transform.position);
             tempManager.ModifyTemperature(temperatura, (intensidad + (potencia * 0.5f)));
 
         }
     }
+
+    public bool isPlayerOn;
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+
+            _audioSource.volume = 0.5f;
+            isPlayerOn = true;
+            if (temperatura > 0)
+            {
+                _audioSource.PlayOneShot(_audioClipHot);
+            }
+            else if (temperatura < 0)
+            {
+                _audioSource.PlayOneShot(_audioClipFrost);  
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayerOn = false;
+            StartCoroutine(TestCoroutine());
+        }
+    }
     
+    private IEnumerator TestCoroutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        if (!isPlayerOn)
+        {
+            float startVolume = _audioSource.volume;
+            while (_audioSource.volume > 0)
+            {
+                _audioSource.volume -= startVolume * Time.deltaTime / 0.5f;
+                yield return new WaitForFixedUpdate();
+
+            }
+            //_audioSource.Stop();
+        }
+        
+
+    }
     
     
 }
