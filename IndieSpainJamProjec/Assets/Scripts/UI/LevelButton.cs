02e27ab0ef5e1ff93d1f12d577492d3ba2995e98
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2022-09-26 14:00:30 +0200

    Candados en el selector de niveles

--- IndieSpainJamProjec/Assets/Scripts/UI/LevelButton.cs
+++ IndieSpainJamProjec/Assets/Scripts/UI/LevelButton.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelButton : MonoBehaviour
 {
-    [Header("[References]")]
+    public Button levelButton;
+    public GameObject locker, textoNivel;
     public string levelName;
 
+    private void OnEnable()
+    {
+        PlayerPrefs.SetInt("Level 1", 1);
+
+        if (PlayerPrefs.GetInt(levelName, 0) == 1) //El nivel es accesible
+        {
+            levelButton.interactable = true;
+            locker.SetActive(false);
+            textoNivel.SetActive(true);
+        }
+        else
+        {
+            levelButton.interactable = false;
+            locker.SetActive(true);
+            textoNivel.SetActive(false);
+        }
+    }
 
     public void OnClick_Level()
     {
