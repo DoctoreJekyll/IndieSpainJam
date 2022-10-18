daf1cec3afb1d5644f186647722e0c3aea2ed8fe
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-10-18 17:02:56 +0200

    Sistema basico multidialogo

--- IndieSpainJamProjec/Assets/Scripts/Managers/LocaleSelector.cs
+++ IndieSpainJamProjec/Assets/Scripts/Managers/LocaleSelector.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Localization.Settings;;
+using UnityEngine.Localization.Settings;
 
 public class LocaleSelector : MonoBehaviour
 {
+    private bool active;
+
+    public void ChangeLocale(int localeID)//Llamamos a este metodo para cambiar el idioma, el 0 es ingles, el 1 español
+    {
+        if (active)
+        {
+            return;
+        }
+
+        StartCoroutine(SetLocale(localeID));
+
+    }
+
+    IEnumerator SetLocale(int localeID)
+    {
+        active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        active = false;
+    }
     
 
 }
