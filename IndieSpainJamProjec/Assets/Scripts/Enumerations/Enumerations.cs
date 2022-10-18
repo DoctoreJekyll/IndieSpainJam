3362b4474e72c94599f76b8b90f0a727bb5de5f9
ixmas93 <44982516+ixmas93@users.noreply.github.com>
2022-10-18 15:04:38 +0200

    Minor Fix
    He eliminado una carpeta y un script que finalmente no he usado

--- IndieSpainJamProjec/Assets/Scripts/Enumerations/Enumerations.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Enumerations : MonoBehaviour
-{
-    //public enum Element { ICE, WATER, FIRE }
-    //public enum MatterState { SOLID, LIQUID, GAS }
-}
