63baab0186a74553446e85be65f71fbdbcc5a42a
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-11-07 14:37:21 +0100

    Comportamiento camara mirar arriba y abajo, por ahora solo en mi escena

---
+++ IndieSpainJamProjec/Assets/Scripts/Managers/CinemachineSwitch.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CinemachineSwitch : MonoBehaviour
+{
+    [SerializeField] private InputAction action;
+    private Animator animator;
+    private Vector2 directionValue;
+    private float value;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        
+        action.Enable();
+        action.performed += ctx => directionValue = ctx.ReadValue<Vector2>();
+        action.canceled += ctx => directionValue = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        action.Disable();
+    }
+
+    private void Update()
+    {
+        value = directionValue.y;
+        
+        Debug.Log(value);
+        
+        SwitchesBetweenCameras();
+    }
+
+    private void SwitchesBetweenCameras()
+    {
+        if (value > 0.1f)
+        {
+            animator.Play("TOPCamera");
+            Debug.Log("Camara top");
+        }
+        else if (value < -0.1f)
+        {
+            animator.Play("BOTCamera");
+            Debug.Log("Camara bot");
+        }
+        else
+        {
+            animator.Play("MainCamera");
+            Debug.Log("Pon la camra normal dfsf");
+        }
+    }
+    
+}
