4ce5adc3c4d6b9cb0afc72e5acb4fb805594c249
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-11-07 17:37:24 +0100

    Tengo algunos errores con el mover de la cámara, dejo comentarios
    Si te mueves no puedes hacer zoom  hacia arriba, pero si haces zoom y te mueves se mantiene

--- IndieSpainJamProjec/Assets/Scripts/Managers/CinemachineSwitch.cs
+++ IndieSpainJamProjec/Assets/Scripts/Managers/CinemachineSwitch.cs
@@ -19,11 +19,6 @@ public class CinemachineSwitch : MonoBehaviour
         action.canceled += ctx => directionValue = Vector2.zero;
     }
 
-    private void OnEnable()
-    {
-
-    }
-
     private void OnDisable()
     {
         action.Disable();
@@ -32,9 +27,13 @@ public class CinemachineSwitch : MonoBehaviour
     private void Update()
     {
         value = directionValue.y;
+        Debug.Log(directionValue.x);
+        Debug.Log(directionValue.y);
         SwitchesBetweenCameras();
     }
 
+    //TODO Ahora mismo a pesar de que la X no sea 0, reproduce la animacion de cambio de cámara
+    //La X vale 0 cuando no deberia, probar a generar los inputs en el input system y no en un action dentro de un script
     private void SwitchesBetweenCameras()
     {
         if (value > 0.9f && directionValue.x == 0)
