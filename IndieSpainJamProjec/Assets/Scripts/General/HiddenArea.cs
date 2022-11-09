7ea899a26666b4efc9b3c27ee933133e080b1580
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-11-09 13:22:28 +0100

    Area secreta base
    Zona con un trigger oscura, cuando entras el alpha se reduce a 0, cuando sales vuelve a 1, esto es modificable.

---
+++ IndieSpainJamProjec/Assets/Scripts/General/HiddenArea.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenArea : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private float alphaValue = 1f;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        spriteRenderer.color =
+            new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, alphaValue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(SetHiddenAlphaToZero());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(SetHiddenAlphaToOne());
+        }
+    }
+
+    IEnumerator SetHiddenAlphaToZero()
+    {
+        while (spriteRenderer.color.a > 0)
+        {
+            alphaValue -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+    
+    IEnumerator SetHiddenAlphaToOne()
+    {
+        while (spriteRenderer.color.a < 1)
+        {
+            alphaValue += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
