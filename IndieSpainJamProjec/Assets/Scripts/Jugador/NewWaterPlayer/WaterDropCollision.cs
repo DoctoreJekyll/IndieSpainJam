18e5fac1f4c89f9cbb640de3fd5746beacac1be0
Joseajierro <46693012+DoctoreJekyll@users.noreply.github.com>
2022-12-09 14:15:32 +0100

    Lanzar la particula dentro de los inputs

--- IndieSpainJamProjec/Assets/Scripts/Jugador/NewWaterPlayer/WaterDropCollision.cs
+++ IndieSpainJamProjec/Assets/Scripts/Jugador/NewWaterPlayer/WaterDropCollision.cs
@@ -1,8 +1,33 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WaterDropCollision : MonoBehaviour
 {
-    
+
+    private ParticleSystem particleSystem;
+
+    private void Awake()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Stop();
+    }
+
+    public void LaunchWater(InputAction.CallbackContext context)//Llamamos a este metodo dentro del componente input action del playermanager 
+    {
+        if (context.performed)
+        {
+            particleSystem.Play();
+            Debug.Log("PlayParticle");
+        }
+        else if (context.canceled)
+        {
+            particleSystem.Stop();
+            Debug.Log("stopParticle");
+        }
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         if (other.gameObject.CompareTag("Activable"))
